a634067dc3b4fb93904641e9a8f9d2f3a1fc4838
Bovard Tiberi <bovard.tiberi@gmail.com>
2016-01-05 14:34:39 -0800

    Added a couple more functions for clarity

--- team037/Utilites/RubbleUtilities.java
+++ team037/Utilites/RubbleUtilities.java
@@ -9,11 +9,27 @@ public class RubbleUtilities
     /**
      * This method returns the number of clear actions necessary to remove rubble
      */
-    public static int calculateClearActionsToClear(double rubble)
+    public static int calculateClearActionsToReduceToZero(double rubble)
     {
         double I = GameConstants.RUBBLE_CLEAR_PERCENTAGE;
         double W = GameConstants.RUBBLE_CLEAR_FLAT_AMOUNT;
 
         return (int)Math.ceil(Math.log10(W / (W - rubble * (I - 1))) / Math.log10(I));
+    }
+
+    public static int calculateClearActionsToReduceBelow100(double rubble) {
+        if (rubble < 100) {
+            return 0
+        } else {
+            return Math.max(calculateClearActionsToReduceToZero(rubble) - 8, 1);
+        }
+    }
+
+    public static int calculateClearActionsToReduceBelow50(double rubble) {
+        if (rubble < 50) {
+            return 0
+        } else {
+            return Math.max(calculateClearActionsToReduceToZero(rubble) - 4, 1)
+        }
     }
 }
