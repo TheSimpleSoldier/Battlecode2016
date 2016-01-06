c5c568e534271054513bd9c422031417f5939afa
Fred Kneeland <fred@Freds-iMac.local>
2016-01-05 21:58:01 -0700

    wokring on using trained neural net

--- team037/Units/BaseArchon.java
+++ team037/Units/BaseArchon.java
@@ -33,13 +33,20 @@ public class BaseArchon extends Unit
     // maybe spawn a unit?
     public boolean carryOutAbility() throws GameActionException
     {
-        if (rc.hasBuildRequirements(RobotType.SOLDIER) && rc.getCoreDelay() < 1)
+        if (rc.hasBuildRequirements(RobotType.SOLDIER) && rc.hasBuildRequirements(RobotType.SOLDIER) && rc.getCoreDelay() < 1)
         {
             for (int i = dirs.length; --i>=0; )
             {
-                if (rc.canBuild(dirs[i], RobotType.SOLDIER))
+                double choice = Math.random();
+                if (choice < 0.5 && rc.canBuild(dirs[i], RobotType.SOLDIER))
                 {
                     rc.build(dirs[i], RobotType.SOLDIER);
+                    return true;
+                }
+                else if (choice > 0.5 && rc.canBuild(dirs[i], RobotType.GUARD))
+                {
+                    rc.build(dirs[i], RobotType.GUARD);
+                    return true;
                 }
             }
         }
