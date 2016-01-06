cbe73bfaf6a139c7bc82cd6ad29579d8a4df4b06
Bovard Tiberi <bovard.tiberi@gmail.com>
2016-01-05 17:25:18 -0800

    AlphaArchon now hunts parts

--- team037/Units/AlphaArchon.java
+++ team037/Units/AlphaArchon.java
@@ -6,16 +6,35 @@ import battlecode.common.GameActionException;
 import team037.Unit;
 import team037.SlugNavigator;
 import team037.Utilites.PartsUtilities;
+import team037.Utilites.AppendOnlyMapLocationArray;
 
 import java.lang.Override;
 
 public class AlphaArchon extends Unit
 {
     SlugNavigator move;
+    AppendOnlyMapLocationArray parts;
+    int partsIdx;
+
     public AlphaArchon(RobotController rc)
     {
         super(rc);
         move = new SlugNavigator(rc);
+        parts = PartsUtilities.findPartsICanSenseNotImpassible(rc);
+        partsIdx = 0;
+        move.setTarget(getNextPartLocation());
+    }
+
+    private MapLocation getNextPartLocation() {
+        if (partsIdx >= parts.length) {
+            parts = PartsUtilities.findPartsICanSenseNotImpassible(rc);
+        }
+        if (parts.length == 0) {
+            return null;
+        }
+        MapLocation nextParts = parts.array[partsIdx];
+        partsIdx += 1;
+        return nextParts;
     }
 
     public boolean takeNextStep() throws GameActionException
@@ -43,6 +62,9 @@ public class AlphaArchon extends Unit
     public boolean act() throws GameActionException {
         if (!rc.isCoreReady()) {
             return false;
+        }
+        if(rc.getLocation().equals(move.getTarget())) {
+            move.setTarget(getNextPartLocation());
         }
         move.takeNextStep();
         return true;
