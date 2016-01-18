49bb9ba193c3c8182796029fdf83b6c6f008d46c
fredkneeland <fred@bladelinux.com>
2016-01-17 21:56:18 -0700

    working on things

--- team037/Utilites/ZombieTracker.java
+++ team037/Utilites/ZombieTracker.java
@@ -11,6 +11,7 @@ public class ZombieTracker
     private static ZombieCount[] zombieCounts;
     private static int currentSpawnRound;
     private static int nextSpawnRound;
+    private static int index;
 
     public ZombieTracker(RobotController robotController)
     {
@@ -18,10 +19,35 @@ public class ZombieTracker
         ZombieSpawnSchedule zombieSpawnSchedule = rc.getZombieSpawnSchedule();
         zombieRounds = zombieSpawnSchedule.getRounds();
         zombieCounts = zombieSpawnSchedule.getScheduleForRound(zombieRounds[0]);
+
+        if (zombieRounds.length > 1)
+        {
+            currentSpawnRound = zombieRounds[0];
+            nextSpawnRound = zombieRounds[1];
+            index = 1;
+        }
     }
 
     public int getNextZombieRound()
     {
+        int currentRound = rc.getRoundNum();
 
+        while (currentRound > currentSpawnRound)
+        {
+            currentSpawnRound = nextSpawnRound;
+            index++;
+
+            // if there are no more spawn rounds
+            if (index >= zombieRounds.length)
+            {
+                return Integer.MAX_VALUE;
+            }
+
+            nextSpawnRound = zombieRounds[index];
+        }
+
+        return currentSpawnRound;
     }
+
+    public int getNextZombieRoundStrength()
 }
