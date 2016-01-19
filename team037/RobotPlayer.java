4d1a98f2fbac2d775714b114db7003a22ab50002
Joshua Leger <legermjoshua@gmail.com>
2016-01-19 10:22:38 -0700

    fixed stuff

--- team037/RobotPlayer.java
+++ team037/RobotPlayer.java
@@ -6,21 +6,18 @@ import battlecode.common.RobotType;
 import team037.DataStructures.SimpleRobotInfo;
 import team037.Enums.Bots;
 import team037.Enums.Strategies;
-import team037.Units.*;
+import team037.Units.AlphaArchon;
 import team037.Units.BaseUnits.*;
 import team037.Units.CastleUnits.CastleArchon;
 import team037.Units.CastleUnits.CastleSoldier;
 import team037.Units.CastleUnits.CastleTurret;
 import team037.Units.DenKillers.DenKillerSoldier;
+import team037.Units.PacMan.PacManArchon;
 import team037.Units.PacMan.PacManGuard;
-import team037.Units.PacMan.PacManScout;
-import team037.Units.PacMan.PacManViper;
-import team037.Units.Rushers.RushingViper;
 import team037.Units.ScoutBomb.ScoutBombArchon;
 import team037.Units.ScoutBomb.ScoutBombGuard;
 import team037.Units.ScoutBomb.ScoutBombScout;
 import team037.Units.TurtleUnits.TurtleArchon;
-import team037.Units.PacMan.PacManArchon;
 
 public class RobotPlayer
 {
@@ -33,103 +30,132 @@ public class RobotPlayer
      **/
     public static void run(RobotController rc)
     {
-        // this will check your ./bc.conf file for a line like this:
-        // bc.testing.strat=foo
-        // and strategy will be foo
-        strategy = System.getProperty("bc.testing.strat");
-        // IT DOESN'T WORK CURRENTLY :(
-        // BUT THEY ARE FIXING IT!
-
-        // hardcode disabled for now
-        strategy = Strategies.SCOUT_BOMB;
-
-
-        RobotType type = rc.getType();
-
-        if (type == RobotType.ARCHON)
-        {
-            if (strategy.equals(Strategies.CASTLE)) {
-                unit = new CastleArchon(rc);
-            } else if (strategy.equals(Strategies.TURTLE)) {
-                unit = new TurtleArchon(rc);
-                Unit.thisBot = Bots.TURTLEARCHON;
-            } else if (strategy.equals(Strategies.SCOUT_BOMB)) {
-                unit = new ScoutBombArchon(rc);
-                Unit.thisBot = Bots.SCOUTBOMBARCHON;
-            } else if (strategy.equals(Strategies.PACMAN)) {
-                unit = new PacManArchon(rc);
-                Unit.thisBot = Bots.PACMANARCHON;
-            } else { // default to alpha archons
-                unit = new AlphaArchon(rc);
-                Unit.thisBot = Bots.ALPHAARCHON;
-            }
-        }
-        else if (type == RobotType.GUARD)
-        {
-            if (strategy.equals(Strategies.PACMAN)) {
-                unit = new PacManGuard(rc);
-                Unit.thisBot = Bots.PACMANGUARD;
-            } else if (strategy.equals(Strategies.SCOUT_BOMB)) {
-                unit = new ScoutBombGuard(rc);
-                Unit.thisBot = Bots.SCOUTBOMBGUARD;
-            } else {
-                unit = new BaseGaurd(rc);
-                Unit.thisBot = Bots.BASEGAURD;
-            }
-        }
-        else if (type == RobotType.SCOUT)
-        {
-            if (strategy.equals(Strategies.PACMAN)) {
-                unit = new ScoutBombScout(rc);
-                Unit.thisBot = Bots.SCOUTBOMBSCOUT;
-            } else if (strategy.equals(Strategies.SCOUT_BOMB)) {
-                unit = new ScoutBombScout(rc);
-                Unit.thisBot = Bots.SCOUTBOMBSCOUT;
-            } else {
-                unit = new BaseScout(rc);
-                Unit.thisBot = Bots.BASESCOUT;
-            }
-        }
-        else if (type == RobotType.SOLDIER)
-        {
-            if (strategy.equals(Strategies.CASTLE)) {
-                unit = new CastleSoldier(rc);
-            } else {
-                unit = new DenKillerSoldier(rc);
-                Unit.thisBot = Bots.DENKILLERSOLDIER;
-            }
-
-        }
-        else if (type == RobotType.TURRET || type == RobotType.TTM)
-        {
-            if (strategy.equals(Strategies.CASTLE)) {
-                unit = new CastleTurret(rc);
-            } else {
-                unit = new BaseTurret(rc);
-                Unit.thisBot = Bots.BASETURRET;
-            }
-        }
-        else if (type == RobotType.VIPER)
-        {
-            unit = new BaseViper(rc);
-            Unit.thisBot = Bots.BASEVIPER;
-        }
-
-        // initial update to strategy
         try
         {
-            unit = unit.getNewStrategy(unit);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+            // this will check your ./bc.conf file for a line like this:
+            // bc.testing.strat=foo
+            // and strategy will be foo
+            strategy = System.getProperty("bc.testing.strat");
+            // IT DOESN'T WORK CURRENTLY :(
+            // BUT THEY ARE FIXING IT!
 
-        if (rc.getRoundNum() == 0 && rc.getType() == RobotType.ARCHON)
-        {
-            BaseArchon.sendOutInitialLocation();
-            BaseArchon.mapKnowledge.addArchon(new SimpleRobotInfo(rc.getID(), rc.getLocation(), RobotType.ARCHON, rc.getTeam()), true);
+            // hardcode disabled for now
+            strategy = Strategies.TURTLE;
+
+
+            RobotType type = rc.getType();
+
+            if(type == RobotType.ARCHON)
+            {
+                if(strategy.equals(Strategies.CASTLE))
+                {
+                    unit = new CastleArchon(rc);
+                }
+                else if(strategy.equals(Strategies.TURTLE))
+                {
+                    unit = new TurtleArchon(rc);
+                    Unit.thisBot = Bots.TURTLEARCHON;
+                }
+                else if(strategy.equals(Strategies.SCOUT_BOMB))
+                {
+                    unit = new ScoutBombArchon(rc);
+                    Unit.thisBot = Bots.SCOUTBOMBARCHON;
+                }
+                else if(strategy.equals(Strategies.PACMAN))
+                {
+                    unit = new PacManArchon(rc);
+                    Unit.thisBot = Bots.PACMANARCHON;
+                }
+                else
+                { // default to alpha archons
+                    unit = new AlphaArchon(rc);
+                    Unit.thisBot = Bots.ALPHAARCHON;
+                }
+            }
+            else if(type == RobotType.GUARD)
+            {
+                if(strategy.equals(Strategies.PACMAN))
+                {
+                    unit = new PacManGuard(rc);
+                    Unit.thisBot = Bots.PACMANGUARD;
+                }
+                else if(strategy.equals(Strategies.SCOUT_BOMB))
+                {
+                    unit = new ScoutBombGuard(rc);
+                    Unit.thisBot = Bots.SCOUTBOMBGUARD;
+                }
+                else
+                {
+                    unit = new BaseGaurd(rc);
+                    Unit.thisBot = Bots.BASEGAURD;
+                }
+            }
+            else if(type == RobotType.SCOUT)
+            {
+                if(strategy.equals(Strategies.PACMAN))
+                {
+                    unit = new ScoutBombScout(rc);
+                    Unit.thisBot = Bots.SCOUTBOMBSCOUT;
+                }
+                else if(strategy.equals(Strategies.SCOUT_BOMB))
+                {
+                    unit = new ScoutBombScout(rc);
+                    Unit.thisBot = Bots.SCOUTBOMBSCOUT;
+                }
+                else
+                {
+                    unit = new BaseScout(rc);
+                    Unit.thisBot = Bots.BASESCOUT;
+                }
+            }
+            else if(type == RobotType.SOLDIER)
+            {
+                if(strategy.equals(Strategies.CASTLE))
+                {
+                    unit = new CastleSoldier(rc);
+                }
+                else
+                {
+                    unit = new DenKillerSoldier(rc);
+                    Unit.thisBot = Bots.DENKILLERSOLDIER;
+                }
+
+            }
+            else if(type == RobotType.TURRET || type == RobotType.TTM)
+            {
+                if(strategy.equals(Strategies.CASTLE))
+                {
+                    unit = new CastleTurret(rc);
+                }
+                else
+                {
+                    unit = new BaseTurret(rc);
+                    Unit.thisBot = Bots.BASETURRET;
+                }
+            }
+            else if(type == RobotType.VIPER)
+            {
+                unit = new BaseViper(rc);
+                Unit.thisBot = Bots.BASEVIPER;
+            }
+
+            // initial update to strategy
+            try
+            {
+                unit = unit.getNewStrategy(unit);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+
+            if(rc.getRoundNum() == 0 && rc.getType() == RobotType.ARCHON)
+            {
+                BaseArchon.sendOutInitialLocation();
+                BaseArchon.mapKnowledge.addArchon(new SimpleRobotInfo(rc.getID(), rc.getLocation(), RobotType.ARCHON, rc.getTeam()), true);
+            }
         }
+        catch(Exception e){e.printStackTrace();}
 
 
         // Game loop that will execute very round
