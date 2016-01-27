48850480ea71a90293099ada843f5abbe6e9ee9a
djb388 <djb229@gmail.com>
2016-01-27 12:20:39 -0700

    removed strategy hardcode

--- team037/RobotPlayer.java
+++ team037/RobotPlayer.java
@@ -58,7 +58,6 @@ public class RobotPlayer
                 strategy = Strategies.RUSH;
             }*/
 
-            strategy = Strategies.PACMAN;
 
             if(rc.getRoundNum() == 0)
             {
